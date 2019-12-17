d7f6d19561e40492bbb197f21dd5604bd6a90c1e
Starai <llstaraill@gmail.com>
2019-12-17 12:04:09 +0100

    AoC Day9: Part I & II

--- turingMachineD9.js
+++ turingMachineD9.js
@@ -16,7 +16,7 @@ function getParameter(mode, num, rB, band, pos) {
       return band[pos + num];
       break;
     case 2:
-      return rB + band[pos + num];
+      return band[rB + band[pos + num]];
       break;
   }
 }
@@ -26,8 +26,14 @@ function* turingCode(band, pos, relativeBase, resultArr) {
     let [op, ...mode] = prepareDP(band[pos]);
     let par1 = getParameter(mode[0], 1, relativeBase, band, pos);
     let par2 = getParameter(mode[1], 2, relativeBase, band, pos);
-    let par3 = mode[2] === 2 ? rB + band[pos + 3] : band[pos + 3];
-    console.log(`Position: ${pos}/${band[pos]} - OP: ${op}`);
+    let par3 = mode[2] === 2 ? relativeBase + band[pos + 3] : band[pos + 3];
+    /*console.log(
+      `Pos: ${pos}/${
+        band[pos]
+      } - rB: ${relativeBase}\nPar: [${par1}|${par2}|${par3}]\nPos:[${
+        band[pos + 1]
+      }|${band[pos + 2]}|${band[pos + 3]}]`
+    );*/
     switch (op) {
       case 99:
         console.log(`99`);
@@ -42,17 +48,25 @@ function* turingCode(band, pos, relativeBase, resultArr) {
         break;
       case 3:
         let inp = yield;
+
+        /*console.log(
+          `band[pos+1] ${band[pos + 1]}\nM2: ${relativeBase +
+            band[pos + 1]}: M1${band[pos + 1]}`
+        );*/
         console.log(`Input: ${inp}`);
-        let three = mode[0] === 2 ? relativeBase+band[pos+1] : band[pos+1]
+        let three =
+          mode[0] == 2
+            ? parseInt(relativeBase) + parseInt(band[pos + 1])
+            : band[pos + 1];
         band.splice(three, 1, inp);
         pos += 2;
         break;
       case 4:
-        console.log(relativeBase)
-        const four =  mode[0] === 2 ? band[relativeBase+band[pos+1]] : band[pos+1]
+        console.log(relativeBase);
+        const four = par1;
         const result = [four, pos + 2, relativeBase];
         console.log(`Output: ${result[0]}`);
-        resultArr.push(four)
+        resultArr.push(four);
         //yield result;
         pos += 2;
         break;
@@ -61,23 +75,20 @@ function* turingCode(band, pos, relativeBase, resultArr) {
 
         break;
       case 6:
-        console.log(`${par1} | ${par2} | ${par3}\n${relativeBase}`)
+        console.log(`${par1} | ${par2} | ${par3}\n${relativeBase}`);
         par1 === 0 ? (pos = par2) : (pos += 3);
         break;
-        case 7:
-          par1 < par2
-            ? band.splice(band[pos + 3], 1, 1)
-            : band.splice(band[pos + 3], 1, 0);
-          pos += 4;
-          break;
-        case 8:
-          par1 === par2
-            ? band.splice(band[pos + 3], 1, 1)
-            : band.splice(band[pos + 3], 1, 0);
-          pos += 4;
-          break;
+      case 7:
+        par1 < par2 ? band.splice(par3, 1, 1) : band.splice(par3, 1, 0);
+        pos += 4;
+        break;
+      case 8:
+        par1 == par2 ? band.splice(par3, 1, 1) : band.splice(par3, 1, 0);
+        pos += 4;
+        break;
       case 9:
-        relativeBase += par1;
+        let nine = par1;
+        relativeBase += nine;
         pos += 2;
         break;
     }
@@ -98,17 +109,17 @@ TuringMachine.prototype.run = function(input) {
   let resultArr = [];
   let op = turingCode(band, pos, base, resultArr);
   let done = op.next().done;
-  /*if (done) {
-    return "Done";
+  if (done) {
+    return resultArr;
   }
   if (Array.isArray(input)) {
     op.next(input[0]);
     result = op.next(input[1]).value;
   } else {
     result = op.next(input).value;
-  }
+  } /*
   this.relativeBase = result[2];
-  this.position = result[1] !== undefined ? result[1] : 0;*/
-
+  this.position = result[1] !== undefined ? result[1] : 0;
+*/
   return resultArr;
 };
