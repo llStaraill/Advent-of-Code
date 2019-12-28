24383dc8494e736d8d6465444e847c3d029f6737
Starai <llstaraill@gmail.com>
2019-12-28 16:41:56 +0100

    Clean Up

--- Day 9/2019_09_01.js
+++
@@ -1,7 +0,0 @@
-
-const additionalMemory = new Array(2000000).fill(0);
-//const boostCode = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99].concat(additionalMemory);
-const boostCode = inputD9.concat(additionalMemory);
-
-let day9 = new TuringMachine(boostCode);
-console.log(day9.run(2));
