fe6de6159d4265506fdad6f3301414e922123167
Friederike Peter <friederike.peter@gmail.com>
2021-04-02 18:42:13 +0200

    Setup + 2015/01

--- 2015/Day 01/01.js
+++ 2015/Day 01/01.js
@@ -4,4 +4,34 @@ const fileName = args[0] === "demo" ? "./demo" : "./input";
 
 let input = fs
   .readFileSync(fileName, "utf-8")
-  .split(/\n/)
+  .split('');
+
+  function countUpsAndDowns(inputArr) {
+    let floor = 0;
+    inputArr.map((cmd, index) => {
+      switch(cmd) {
+        case '(' : floor++ ; break;
+        case ')' : floor-- ; break;
+      }
+    })
+    return floor;
+  }
+
+  const partOne = countUpsAndDowns(input); // 232
+ 
+  function findBasementCmd(inputArr) {
+    let floor = 0;
+    for (let i = 0; i < inputArr.length; i++) {
+      let cmd = inputArr[i];
+      if (cmd === '(') {
+        floor++;
+      } else if (cmd === ')') {
+        floor--
+      }
+      if (floor === -1) {
+        return i+1
+      }
+    }
+  }
+
+  const partTwo = findBasementCmd(input); // 1783
