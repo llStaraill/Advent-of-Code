18878349678abeb90bef9fd01379bc2c9df1c533
Friederike Peter <friederike.peter@gmail.com>
2021-04-02 23:13:37 +0200

    2015/04

--- 2015/Day 04/04.js
+++ 2015/Day 04/04.js
@@ -1,7 +1,31 @@
 const fs = require("fs");
+const crypto = require("crypto");
 const args = process.argv.splice(2);
 const fileName = args[0] === "demo" ? "./demo" : "./input";
 
-let input = fs
-  .readFileSync(fileName, "utf-8")
-  .split(/\n/)
+let input = fs.readFileSync(fileName, "utf-8");
+
+function createHash(string) {
+  return crypto.createHash("md5").update(string).digest("hex");
+}
+
+
+
+function findNumber(string, nr) {
+  let number = 0;
+  let startStringArr = new Array(nr).fill("0");
+  let startString = startStringArr.join("");
+  let currHash;
+
+  do {
+    currHash = createHash(`${string}${number}`);
+    number++;
+  } while (currHash.substring(0, nr) !== startString);
+
+  return number - 1;
+}
+
+//const partOne = findNumber(input, 5); 
+const partTwo = findNumber(input,6);
+console.log(partTwo);
+
