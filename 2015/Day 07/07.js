7e65a5571aa1732b724816ff1c7ca79b0880a3cf
Friederike Peter <friederike.peter@gmail.com>
2021-04-04 18:01:12 +0200

    2015/Day07 Part I WIP

--- 2015/Day 07/07.js
+++ 2015/Day 07/07.js
@@ -5,3 +5,23 @@ const fileName = args[0] === "demo" ? "./demo" : "./input";
 let input = fs
   .readFileSync(fileName, "utf-8")
   .split(/\n/)
+  .map((cmd) => cmd.replace(/\r/, ""));
+
+//console.log(input);
+
+const x = 123;
+const y = 456;
+const d = x & y;
+const e = x | y;
+const f = x << 2;
+const g = y >> 2;
+const h = ~x;
+const i = ~y;
+console.log(`d: ${d}`);
+console.log(`e: ${e}`);
+console.log(`f: ${f}`);
+console.log(`g: ${g}`);
+console.log(`h: ${h}`);
+console.log(`i: ${i}`);
+console.log(`x: ${x}`);
+console.log(`y: ${y}`);
