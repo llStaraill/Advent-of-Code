93d193f4729ba3888401e4c12878784717fdbea9
fpe <friederike.peter@googlemail.com>
2023-12-22 11:45:32 +0100

    2023 - 05 -01

--- 2023/05/05.js
+++ 2023/05/05.js
@@ -13,118 +13,71 @@ const deepCopy = (val) => JSON.parse(JSON.stringify(val));
 
 /** Get Input */
 
-let input = fs.readFileSync(fileName, "utf-8");
+let input = fs.readFileSync(fileName, "utf-8").split("\n\n");
 
-/** Part One */
 
-class Seed {
-  constructor({
-    seed,
-    soil,
-    fertilizer,
-    water,
-    light,
-    temperature,
-    humidity,
-    location,
-  }) {
-    this.seed = seed;
-    this.soil = soil;
-    this.fertilizer = fertilizer;
-    this.water = water;
-    this.light = light;
-    this.temperature = temperature;
-    this.humidity = humidity;
-    this.location = location;
-  }
+const seeds = input[0].replace("seeds: ", "").split(" ").map(Number)
 
-  add(key, val) {
-    this[key] = val;
-  }
+
+
+const findSeedRange = (row, seed) => {
+  const [_, source, range] = row;
+  return source <= seed && seed <= source + range
+
 }
 
-class Almanach {
-  constructor() {
-    this.seeds = [];
+
+const mapSeedToValue = (seed, seedKey, index, seedMap) => {
+  const rows = input[index].split("\n").splice(1).map((row) => row.split(" ").map(Number))
+
+  const seedRange = rows.find((row) => findSeedRange(row, seed))
+
+  let seedTarget = seed;
+
+  if (seedRange) {
+    const [target, source, _] = seedRange;
+
+    seedTarget = target + (seed - source)
+
   }
 
-  addSeed(id) {
-    this.seeds.push(new Seed(id));
+
+
+
+  const seedMapEntry = seedMap.get(seedKey);
+
+  if (seedMapEntry) {
+
+    seedMapEntry.push(seedTarget);
+    seedMap.set(seedKey, seedMapEntry)
+  } else {
+    seedMap.set(seedKey, [seed, seedTarget])
   }
 
-  getSeedBy(key, id) {
-    return this.seeds.find((seed) => seed[key] === id);
-  }
+  const hasNextRow = !!input[index + 1]
+
+  if (hasNextRow) mapSeedToValue(seedTarget, seedKey, index + 1, seedMap)
+
 }
 
-const almanach = new Almanach();
 
-const getStringMapValues = (stringValue, id) =>
-  stringValue
-    .replace(id, "")
-    .split("\n")
-    .filter((el) => el !== "")
-    .map((row) => row.split(" ").map(Number));
 
-const partOneInput = input.split("\n\n");
+const mapSeeds = (seeds, index) => {
+  const seedMap = new Map();
 
-const seeds = partOneInput[0].replace("seeds: ", "").split(" ").map(Number);
 
-const addValueToAlmanach = (values, sourceKey, destinationKey) => {
-  const [destinationStart, sourceStart, range] = values;
+  seeds.map((seed) => mapSeedToValue(seed, seed, 1, seedMap))
 
-  console.log({ sourceKey, destinationKey });
+  // figure out if in Range
 
-  for (let i = 0; i < range; i++) {
-    const almanachSeed = almanach.getSeedBy(sourceKey, sourceStart + i);
 
-    console.log(sourceStart + i, destinationStart + i, almanachSeed);
-    if (almanachSeed) {
-      almanachSeed.add(destinationKey, destinationStart + 1);
-    } else {
-      almanach.addSeed({
-        [sourceKey]: destinationStart + 1,
-        [destinationKey]: destinationStart + i,
-      });
-    }
-  }
-};
 
-const flow = [
-  "seed",
-  "soil",
-  "fertilizer",
-  "water",
-  "light",
-  "temperature",
-  "humidity",
-  "location",
-];
+  return seedMap
 
-for (let i = 0; i < flow.length - 1; i++) {
-  const source = flow[i];
-  const destination = flow[i + 1];
-
-  // console.log({ source, destination });
-  const valueMap = getStringMapValues(
-    partOneInput[i + 1],
-    `${flow[i]}-to-${flow[i + 1]} map:`
-  );
-
-  console.log({ valueMap });
-
-  valueMap.forEach((valueRow) =>
-    addValueToAlmanach(valueRow, source, destination)
-  );
 }
 
-// const almanachSeeds = seeds.map((seed) => {
-//   const almanachSeed = almanach.getSeedBy("seed", seed);
-//   return almanachSeed;
-// });
+const partOneMap = mapSeeds(seeds, 1)
 
-console.log(almanach.getSeedBy("location", 35));
+const partOne = Math.min(...Array.from(partOneMap.values()).map(val => val[val.length - 1]))
 
-// console.log(almanach);
-
-/** Part Two */
+console.log({ partOne })
